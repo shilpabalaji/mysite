705aac71698d16a403364e6ab34690230093894f
Shilpa Balaji <shilpa@shilpabalaji.com>
2013-06-08 13:48:10 -0800

    changed with text

--- web.js
+++ web.js
@@ -2,9 +2,7 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-file = fopen(getScriptPath("index.html"), o);
-content = fread(file, flength(file));
-fclose(file);
+content = '<html><head><title>Shilpa Balaji</title><link rel=stylesheet href="css/style.css" type="text/css"></head><body><div class="boxed"><h1 class="name">Shilpa Balaji</h1> <hr><hr><div class="main_text"><br>email me: <a href="mailto:shilpa@shilpabalaji.com">shilpa@shilpabalaji.com</a><br></div></div></body><html>'
 
 app.get('/', function(request, response) {
   response.send(content);
