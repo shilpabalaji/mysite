d30248cf874d09f619c68fac5f62152965b37568
Shilpa Balaji <shilpa@shilpabalaji.com>
2013-06-09 02:28:21 -0800

    contact added

--- www/scripts.js
+++ www/scripts.js
@@ -12,8 +12,21 @@ function resumeclick() {
 }
 
 function aboutclick() {
-  console.log("home clicked");
+  console.log("about clicked");
   var text = document.getElementById("main_text");
   text.innerHTML = 
-    "Well. I just graduated from <a href = 'http://www.cmu.edu'>Carnegie Mellon University</a> with a degree in Computer Science. Pretty soon I'll be working as a Forward Deployed Engineer at <a href='http://www.palantir.com'>Palantir Technologies</a>. Until then, I'm spending a few months up in the glorious Seward, Alaska.<br><br>I like to read, write, and paint (among other things) when I'm not computer sciencing. I also like meeting new people, so go ahead and drop me a line if you're so inclined."
+    "Well. I just graduated from <a href = 'http://www.cmu.edu'>Carnegie Mellon University</a> with a degree in Computer Science. Pretty soon I'll be working as a Forward Deployed Engineer at <a href='http://www.palantir.com'>Palantir Technologies</a>. Until then, I'm spending a few months up in the glorious Seward, Alaska.<br><br>I like to read, write, and paint (among other things) when I'm not computer sciencing. I also like meeting new people, so go ahead and drop me a line if you're so inclined.";
+}
+
+function contactclick() {
+  console.log("about clicked");
+  var text = document.getElementById("main_text");
+  text.innerHTML = "I'm all over the place. Find me at:<br><br><center>" + 
+    "<a href='https://www.facebook.com/shilpa.balaji'>facebook</a><br>" +
+    "<a href='www.quora.com/Shilpa-Balaji'>quora</a><br>" +
+    "<a href='www.linkedin.com/in/shilpabalaji'>linkedin</a><br>" +
+    "<a href='https://github.com/shilpabalaji'>github</a><br>" + 
+    "<a href='https://twitter.com/shilpabalaji'>twitter</a>"
+     ;
+
 }
