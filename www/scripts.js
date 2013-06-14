ad8887cc0d4aec14ad4fe47d2da539b1ac05f5fb
Shilpa Balaji <shilpa@shilpabalaji.com>
2013-06-13 23:58:02 -0800

    edited fonts, new contact page

--- www/scripts.js
+++ www/scripts.js
@@ -1,7 +1,5 @@
 // lower menu buttons
 
-document.onload = aboutclick();
-
 function resumeclick() {
   console.log("resume clicked");
   window.location = "/resume.pdf";
@@ -11,19 +9,20 @@ function aboutclick() {
   console.log("about clicked");
   var text = document.getElementById("main_text");
   text.innerHTML = 
-    "Hi. I'm Shilpa. I just graduated from <a href = 'http://www.cmu.edu'>Carnegie Mellon University</a> with a degree in Computer Science. Pretty soon I'll be working as a Forward Deployed Engineer at <a href='http://www.palantir.com'>Palantir Technologies</a>. Until then, though, I'm spending a few months up in glorious Seward, Alaska. <br><br>  I like to write, read, and paint (among other things) when I'm not working. I also like meeting new people, so go ahead and shoot me an <a href='mailto:shilpa@shilpabalaji.com'>email</a> if you're so inclined. I'd like that."
+    "I just graduated from <a href = 'http://www.cmu.edu'>Carnegie Mellon University</a> with a degree in Computer Science. Pretty soon I'll be working as a Forward Deployed Engineer at <a href='http://www.palantir.com'>Palantir Technologies</a>. Until then, I'm spending a few months up in glorious Seward, Alaska."
 }
 
 function contactclick() {
   console.log("about clicked");
   var text = document.getElementById("main_text");
-  text.innerHTML = "I'm all over the place. Find me at:<br><br>" + 
-    "- <a href='https://www.facebook.com/shilpa.balaji'>facebook</a><br>" +
-    "- <a href='http://www.quora.com/Shilpa-Balaji'>quora</a><br>" +
-    "- <a href='http://www.linkedin.com/in/shilpabalaji'>linkedin</a><br>" +
-    "- <a href='https://github.com/shilpabalaji'>github</a><br>" + 
-    "- <a href='https://twitter.com/shilpabalaji'>twitter</a><br><br>" +
-    "Or, just good old fashioned <a href='mailto:shilpa@shilpabalaji.com'>email</a>."
-     ;
+  text.innerHTML = 
+    "<div><div id='facewrapper'> <img id='face' height=200 width=230 src='./imgs/face.jpg'></div><div id='contacttext'>hihi</div></div>"
+
+}
+
+function homeclick() {
+  console.log("home clicked");
+  var text = document.getElementById("main_text");
+  text.innerHTML = ""
 
 }
